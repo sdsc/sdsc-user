5762b2b8c4d8845997d06cf5bd742ac18861f760
Glenn K. Lockwood <glock@sdsc.edu>
2013-11-29 10:32:49 -0800

    fixed a bug with the bundler scripts on Gordon

--- bundler/bundler.py
+++ bundler/bundler.py
@@ -2,7 +2,7 @@
 ################################################################################
 #  bundler.py - Pack multiple serial jobs into a single job script
 #
-#  September 2013              Glenn K. Lockwood, San Diego Supercomputer Center
+#  Glenn K. Lockwood, San Diego Supercomputer Center             November 2013
 ################################################################################
 import os
 import sys
@@ -20,7 +20,7 @@ hostname = socket.gethostname()
 if hostname.startswith( 'trestles' ):
     sys.path.append(r'/home/diag/opt/mpi4py/1.2.2/lib64/python2.4/site-packages')
 elif hostname.startswith( 'gcn' ) or hostname.startswith( 'gordon' ):
-    sys.path.append(r'/opt/amber/bin')
+    sys.path.append(r'/home/diag/opt/mpi4py/1.3.1/lib/python2.7/site-packages')
 
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
@@ -36,6 +36,8 @@ TF.close()
 for i in range(ntasks):
     if myid == (i % numprocs):
         task = tasks[i]
-        task.rstrip()
-        print "MPI process %d on %s running task [%s]" % (myid, hostname, task.strip())
+        task = task.rstrip()
+        print "MPI process %d on %s running task [%s]" % (myid, hostname, task)
         os.system(task)
+
+MPI.Finalize()
