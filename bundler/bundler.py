dd51545dd98be86b7b505514b10a8f074dc144f1
Glenn K. Lockwood <glock@sdsc.edu>
2014-04-11 15:41:24 -0700

    added support for using pbsdsh instead of mpirun

--- bundler/bundler.py
+++ bundler/bundler.py
@@ -14,20 +14,35 @@ try:
 except IOError:
     sys.stderr.write( "Unable to open tasks file %s\n" % tasksfile )
 
-### Initialize MPI information
 hostname = socket.gethostname()
 
-if hostname.startswith( 'trestles' ):
-    sys.path.append(r'/home/diag/opt/mpi4py/mvapich2/gnu/1.3.1/lib/python')
-elif hostname.startswith( 'gcn' ) or hostname.startswith( 'gordon' ):
-    sys.path.append(r'/home/diag/opt/mpi4py/mvapich2/intel/1.3.1/lib/python')
+### We can use pbsdsh instead of MPI if the bundled tasks themselves need MPI.
+### Using Torque's TM interface to launch tasks is very ugly because they start
+### with bare environments.  This is how you might want to run:
+### $ pbsdsh /usr/bin/env BUNDLER_MODE=pbsdsh PBS_NP=$PBS_NP PATH=$PATH \
+###                       LD_LIBRARY_PATH=$LD_LIBRARY_PATH $(which python-mpi) \
+###                       $PWD/bundler.py $PWD/TASKS
+if ('BUNDLER_MODE' in os.environ 
+and os.environ['BUNDLER_MODE'].lower() == "pbsdsh"):
+    myid = int(os.environ['PBS_VNODENUM'])
+    numprocs = int(os.environ['PBS_NP'])
+    taskmgr = "pbsdsh worker"
 
-from mpi4py import MPI
-comm = MPI.COMM_WORLD
-myid = comm.Get_rank()
-numprocs = comm.Get_size()
+### Otherwise use MPI.  Initialize it here
+else:
+    ### Initialize MPI information
+    if hostname.startswith( 'trestles' ):
+        sys.path.append(r'/home/diag/opt/mpi4py/mvapich2/gnu/1.3.1/lib/python')
+    elif hostname.startswith( 'gcn' ) or hostname.startswith( 'gordon' ):
+        sys.path.append(r'/home/diag/opt/mpi4py/mvapich2/intel/1.3.1/lib/python')
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    myid = comm.Get_rank()
+    numprocs = comm.Get_size()
+    taskmgr = "MPI process"
 
 ### Read in tasks to be executed
+print "Got %d task slots" % numprocs
 tasks = TF.readlines()
 ntasks = len(tasks)
 TF.close()
@@ -37,7 +52,10 @@ for i in range(ntasks):
     if myid == (i % numprocs):
         task = tasks[i]
         task = task.rstrip()
-        print "MPI process %d on %s running task [%s]" % (myid, hostname, task)
+        print "%s %d on %s running task [%s]" % (taskmgr, myid, hostname, task)
         os.system(task)
 
-MPI.Finalize()
+try:
+    MPI.Finalize()
+except NameError:
+    pass
